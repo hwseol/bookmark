be669f3b357adf6f1b774d5ae196ec96d1ce4f5e
hwseol <mhmh2090@gmail.com>
2020-11-08 20:03:22 +0900

    add static_root

--- config/settings.py
+++ config/settings.py
@@ -123,5 +123,5 @@ ALLOWED_HOSTS = ['*']
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_files')
 STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
